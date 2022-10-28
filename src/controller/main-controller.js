53a5c3529f81e4d500c0cbcbcd10295ea383f86f
rootphantomer <rootphantomy@hotmail.com>
2022-10-28 14:29:36 +0800

    fix  kook login black page bug

--- src/controller/main-controller.js
+++ src/controller/main-controller.js
@@ -1,4 +1,11 @@
-const { app, BrowserWindow, session, shell, ipcMain } = require("electron");
+const {
+    app,
+    BrowserWindow,
+    BrowserView,
+    session,
+    shell,
+    ipcMain,
+} = require("electron");
 const path = require("path");
 const CssInjector = require("../js/css-injector");
 
@@ -6,143 +13,146 @@ const CssInjector = require("../js/css-injector");
 const UOS_patch = true;
 // https://github.com/BlueSky-07/wechat-token/issues/1
 const UOS_secret =
-  "Gp8ICJkIEpkICggwMDAwMDAwMRAGGoAIP2dyZlmKevRGDZKoJMMXqdPRe1Iv+vL/2Kj8M/Fx/NMXVTdgTfDVuS5QjUjfXzzkMkxWm0Z1kfL7obGFtXzmgUIKp4RUJn6ehaLuPe4Ne4Gjom4/mifCGjNNtrgO20s4We3jo3FAqhcblYdW4vOTPytXB9HTHScBev1aPT66nS8JfneWgE07oCqB6WrgotoXYFKAJX9iIJbMQFUI8JrnJi3kGqpMjunQSb/npt+TEkgFo7hof7tKhHZHLGm2qyL6d8CweDwprnYWhzey9act6YJydLRlAjXaN35dm/6j8OvEct+H65lwT0hyPN28/Q2GkNcaxvB2NhaM80k3Jck7UHtCq2GLnTEMAV80sKSqTl/EWHNQ1G7APuCRXUvD8JOrmSpRARlUmWEWHeXeeZ0a8Qx+BPkyLxvn1RNtWvnI5hknPSMQ5CUaaBHKSaOwtxuB4fpZXJV2wCah0ACLU+7LFZ5bz1LdEkktzGRyC8GkL8MZwk25dgIiHOVSWgfLBRc4mkG4FMBsmJoCsKxG528Yt8fO0kLGb0pbkWZ2xhjfdveGd3UKwkLypOZ5POpZxbNqlYAQay15Zt0/i4oZnOSWZpCY2H8TtspWpHkVfjR2OpViOgSQGU0gHCQZLQf6hw9w8icKg64zhG82NqUm5/DLIBTq4+Q1MAdSoUmdzNfh+gu8te/ouxTwHF7+83cvXvuWyvONLNdTtOmswIcTwANlFIGCxrv+GKA7FbVapvamJ2c3XCg56xWuUnhaxX9SFFVPeVFLkxHH7pmLvyuWjmTDsVhbPUrHTBN+39tdE1MrpmoQlM+zDcV7G4my39n/8omfCr7UNeqE+nAgTyI31O6QlD/onR4QY5le4lYRWllmprj8O9jNnMWDnufG/2SYzaitCOUa/YL/sI0L0a+HFqNWD8FPApNGBw2bQu0JDyi0Ks4IDf0s9xIVSg4MC70Kpxmb01VnJbihU1IligbnO6roqY0ErBFW8bMCJoOWcaAu35kHVp1y8SMIsp815tkNwvdipWO9qAQch6CbQMFle0REj2FI7cpG0fjlr8jO6FFh8e5P4EpBTq9uHnPy1SrlCL0zdf0DPKddUErATZlEjz+64oDbwqfPYb7ECE2me+Pq+gNH2whFDGkdfsdMHJPP2uHFHZEceIfe7QHkkbMW2h4zTojWJFVPqHCPcUYgsnfY9GuGVoR5MG9cvRt8wa1r0MVXbR/Iz/pKL/qKz4IHNPKvjhegI57VHbAomXk9pAaIPx2M/ZfJC3ZSrskfHwp7P/jybzZbjn9XLDPNp1Vi/ANhT65z67lCtzXG/WW9eXG2RZFwlUdijzDL3uCbBOFWLy28FHJcLHEa37sKVX/nYnpcNiDepZiCBigFMAA=";
+    "Gp8ICJkIEpkICggwMDAwMDAwMRAGGoAIP2dyZlmKevRGDZKoJMMXqdPRe1Iv+vL/2Kj8M/Fx/NMXVTdgTfDVuS5QjUjfXzzkMkxWm0Z1kfL7obGFtXzmgUIKp4RUJn6ehaLuPe4Ne4Gjom4/mifCGjNNtrgO20s4We3jo3FAqhcblYdW4vOTPytXB9HTHScBev1aPT66nS8JfneWgE07oCqB6WrgotoXYFKAJX9iIJbMQFUI8JrnJi3kGqpMjunQSb/npt+TEkgFo7hof7tKhHZHLGm2qyL6d8CweDwprnYWhzey9act6YJydLRlAjXaN35dm/6j8OvEct+H65lwT0hyPN28/Q2GkNcaxvB2NhaM80k3Jck7UHtCq2GLnTEMAV80sKSqTl/EWHNQ1G7APuCRXUvD8JOrmSpRARlUmWEWHeXeeZ0a8Qx+BPkyLxvn1RNtWvnI5hknPSMQ5CUaaBHKSaOwtxuB4fpZXJV2wCah0ACLU+7LFZ5bz1LdEkktzGRyC8GkL8MZwk25dgIiHOVSWgfLBRc4mkG4FMBsmJoCsKxG528Yt8fO0kLGb0pbkWZ2xhjfdveGd3UKwkLypOZ5POpZxbNqlYAQay15Zt0/i4oZnOSWZpCY2H8TtspWpHkVfjR2OpViOgSQGU0gHCQZLQf6hw9w8icKg64zhG82NqUm5/DLIBTq4+Q1MAdSoUmdzNfh+gu8te/ouxTwHF7+83cvXvuWyvONLNdTtOmswIcTwANlFIGCxrv+GKA7FbVapvamJ2c3XCg56xWuUnhaxX9SFFVPeVFLkxHH7pmLvyuWjmTDsVhbPUrHTBN+39tdE1MrpmoQlM+zDcV7G4my39n/8omfCr7UNeqE+nAgTyI31O6QlD/onR4QY5le4lYRWllmprj8O9jNnMWDnufG/2SYzaitCOUa/YL/sI0L0a+HFqNWD8FPApNGBw2bQu0JDyi0Ks4IDf0s9xIVSg4MC70Kpxmb01VnJbihU1IligbnO6roqY0ErBFW8bMCJoOWcaAu35kHVp1y8SMIsp815tkNwvdipWO9qAQch6CbQMFle0REj2FI7cpG0fjlr8jO6FFh8e5P4EpBTq9uHnPy1SrlCL0zdf0DPKddUErATZlEjz+64oDbwqfPYb7ECE2me+Pq+gNH2whFDGkdfsdMHJPP2uHFHZEceIfe7QHkkbMW2h4zTojWJFVPqHCPcUYgsnfY9GuGVoR5MG9cvRt8wa1r0MVXbR/Iz/pKL/qKz4IHNPKvjhegI57VHbAomXk9pAaIPx2M/ZfJC3ZSrskfHwp7P/jybzZbjn9XLDPNp1Vi/ANhT65z67lCtzXG/WW9eXG2RZFwlUdijzDL3uCbBOFWLy28FHJcLHEa37sKVX/nYnpcNiDepZiCBigFMAA=";
 
 class MainController {
-  constructor() {
-    !UOS_patch ||
-      session.defaultSession.webRequest.onBeforeSendHeaders(
-        (details, callback) => {
-          details.requestHeaders["extspam"] = UOS_secret;
-          callback({ requestHeaders: details.requestHeaders });
+    constructor() {
+        // !UOS_patch ||
+        //   session.defaultSession.webRequest.onBeforeSendHeaders(
+        //     (details, callback) => {
+        //       details.requestHeaders["extspam"] = UOS_secret;
+        //       callback({ requestHeaders: details.requestHeaders });
+        //     }
+        //   );
+        this.init();
+    }
+
+    init() {
+        this.window = new BrowserWindow({
+            transparent: true,
+            show: false,
+            width: 1920,
+            height: 1080,
+            frame: false,
+            autoHideMenuBar: true,
+            resizable: true,
+            // icon: path.join(__dirname, '../../build/icons/512x512.png'),
+            webPreferences: {
+                // preload: path.join(__dirname, "src/controller/preload.js"),
+                webSecurity: false,
+                nodeIntegration: false,
+                contextIsolation: false,
+                enableRemoteModule: true,
+            },
+        });
+        this.window.webContents.openDevTools();
+
+        this.window.loadURL("https://www.kookapp.cn/app/login", {
+            userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.52"
+        });
+
+        this.window.webContents.on("dom-ready", () => {
+            //   this.window.webContents.insertCSS(CssInjector.login);
+            //   this.window.webContents.insertCSS(CssInjector.main);
+            //   this.addFontAwesomeCDN();
+            this.changeTitle();
+            //   this.addToggleContactElement();
+            //   this.addUnreadMessageListener();
+            this.show();
+        });
+        // this.window.webContents.on("did-finish-load", () => {
+        //   console.log("finish");
+        // });
+
+        this.window.webContents.on("did-fail-load", () => {
+            console.log("----fail-----");
+            this.window.reload();
+        });
+
+        // triggering when user try to close the play window.
+        this.window.on("close", (e) => {
+            if (this.window.isVisible()) {
+                e.preventDefault();
+                this.window.hide();
+            }
+        });
+
+        this.window.webContents.on("new-window", this.openInBrowser);
+
+        session.defaultSession.webRequest.onCompleted(
+            {
+                urls: [
+                    "https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxinit*",
+                    "https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxinit*",
+                    "https://wx.qq.com/?&lang*",
+                    "https://wx2.qq.com/?&lang*",
+                ],
+            },
+            (details) => this.handleRequest(details)
+        );
+
+        ipcMain.on("resizeWindow", (event, value) => {
+            if (value === "desktop") {
+                this.window.setSize(1000, this.window.getSize()[1], true);
+            } else {
+                this.window.setSize(450, this.window.getSize()[1], true);
+            }
+        });
+    }
+
+    show() {
+        this.window.show();
+        this.window.focus();
+    }
+
+    toggle() {
+        if (this.window.isVisible()) {
+            this.window.hide();
+        } else {
+            this.show();
         }
-      );
-    this.init();
-  }
+    }
 
-  init() {
-    this.window = new BrowserWindow({
-      transparent: true,
-      show: false,
-      width: 1920,
-      height: 1080,
-      frame: false,
-      autoHideMenuBar: true,
-      resizable: true,
-      // icon: path.join(__dirname, '../../build/icons/512x512.png'),
-      webPreferences: {
-        webSecurity: false,
-        nodeIntegration: true,
-        contextIsolation: false,
-      },
-    });
-    this.window.webContents.openDevTools();
-
-    this.window.loadURL("https://www.kookapp.cn/");
-    // this.window.loadURL('https://www.kookapp.cn/app/channels/9019557496487189/7511474327643283')
-
-    this.window.webContents.on("dom-ready", () => {
-      //   this.window.webContents.insertCSS(CssInjector.login);
-      //   this.window.webContents.insertCSS(CssInjector.main);
-      //   this.addFontAwesomeCDN();
-      this.changeTitle();
-      //   this.addToggleContactElement();
-      //   this.addUnreadMessageListener();
-      this.show();
-    });
-    // this.window.webContents.on("did-finish-load", () => {
-    //   console.log("finish");
-    // });
-
-    this.window.webContents.on("did-fail-load", () => {
-      console.log("----fail-----");
-      this.window.reload();
-    });
-
-    // triggering when user try to close the play window.
-    this.window.on("close", (e) => {
-      if (this.window.isVisible()) {
+    openInBrowser(e, url) {
         e.preventDefault();
+        // if the url start with a wechat redirect url, get the real url, decode and open in external browser
+        let redirectUrl = url;
+        if (
+            url.startsWith(
+                "https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxcheckurl?requrl="
+            )
+        ) {
+            const redirectRegexp =
+                /https:\/\/wx\.qq\.com\/cgi-bin\/mmwebwx-bin\/webwxcheckurl\?requrl=(.*)&skey.*/g;
+            redirectUrl = decodeURIComponent(redirectRegexp.exec(url)[1]);
+        }
+        shell.openExternal(redirectUrl);
+    }
+
+    handleRequest(details) {
+        // console.log(details.url)
+        details.url.startsWith("https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxinit") &&
+        this.login();
+        details.url.startsWith(
+            "https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxinit"
+        ) && this.login();
+        details.url.startsWith("https://wx.qq.com/?&lang") && this.logout();
+        details.url.startsWith("https://wx2.qq.com/?&lang") && this.logout();
+    }
+
+    login() {
         this.window.hide();
-      }
-    });
-
-    this.window.webContents.on("new-window", this.openInBrowser);
-
-    session.defaultSession.webRequest.onCompleted(
-      {
-        urls: [
-          "https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxinit*",
-          "https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxinit*",
-          "https://wx.qq.com/?&lang*",
-          "https://wx2.qq.com/?&lang*",
-        ],
-      },
-      (details) => this.handleRequest(details)
-    );
-
-    ipcMain.on("resizeWindow", (event, value) => {
-      if (value === "desktop") {
-        this.window.setSize(1000, this.window.getSize()[1], true);
-      } else {
-        this.window.setSize(450, this.window.getSize()[1], true);
-      }
-    });
-  }
-
-  show() {
-    this.window.show();
-    this.window.focus();
-  }
-
-  toggle() {
-    if (this.window.isVisible()) {
-      this.window.hide();
-    } else {
-      this.show();
+        this.window.setSize(1000, 800, true);
+        this.window.setResizable(true);
+        this.window.show();
     }
-  }
 
-  openInBrowser(e, url) {
-    e.preventDefault();
-    // if the url start with a wechat redirect url, get the real url, decode and open in external browser
-    let redirectUrl = url;
-    if (
-      url.startsWith(
-        "https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxcheckurl?requrl="
-      )
-    ) {
-      const redirectRegexp =
-        /https:\/\/wx\.qq\.com\/cgi-bin\/mmwebwx-bin\/webwxcheckurl\?requrl=(.*)&skey.*/g;
-      redirectUrl = decodeURIComponent(redirectRegexp.exec(url)[1]);
+    logout() {
+        this.window.setSize(380, 500, true);
     }
-    shell.openExternal(redirectUrl);
-  }
 
-  handleRequest(details) {
-    // console.log(details.url)
-    details.url.startsWith("https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxinit") &&
-      this.login();
-    details.url.startsWith(
-      "https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxinit"
-    ) && this.login();
-    details.url.startsWith("https://wx.qq.com/?&lang") && this.logout();
-    details.url.startsWith("https://wx2.qq.com/?&lang") && this.logout();
-  }
-
-  login() {
-    this.window.hide();
-    this.window.setSize(1000, 800, true);
-    this.window.setResizable(true);
-    this.window.show();
-  }
-
-  logout() {
-    this.window.setSize(380, 500, true);
-  }
-
-  addFontAwesomeCDN() {
-    this.window.webContents.executeJavaScript(`
+    addFontAwesomeCDN() {
+        this.window.webContents.executeJavaScript(`
             let faLink = document.createElement('link');
             faLink.setAttribute('rel', 'stylesheet');
             faLink.type = 'text/css';
@@ -151,10 +161,10 @@ class MainController {
             faLink.crossOrigin = 'anonymous';
             document.head.appendChild(faLink);
         `);
-  }
+    }
 
-  changeTitle() {
-    this.window.webContents.executeJavaScript(`
+    changeTitle() {
+        this.window.webContents.executeJavaScript(`
             var titleName = 'Freechat (version: ${app.getVersion()})';
             document.title = titleName;
             new MutationObserver(mutations => {
@@ -163,10 +173,10 @@ class MainController {
                 }
             }).observe(document.querySelector('title'), {childList: true});
         `);
-  }
+    }
 
-  addUnreadMessageListener() {
-    this.window.webContents.executeJavaScript(`
+    addUnreadMessageListener() {
+        this.window.webContents.executeJavaScript(`
             new MutationObserver(mutations => {
                 let unread = document.querySelector('.icon.web_wechat_reddot');
                 let unreadImportant = document.querySelector('.icon.web_wechat_reddot_middle');
@@ -174,10 +184,10 @@ class MainController {
                 require('electron').ipcRenderer.send('updateUnread', unreadType);
             }).observe(document.querySelector('.chat_list'), {subtree: true, childList: true});
         `);
-  }
+    }
 
-  addToggleContactElement() {
-    this.window.webContents.executeJavaScript(`
+    addToggleContactElement() {
+        this.window.webContents.executeJavaScript(`
             let toggleButton = document.createElement('i');
             toggleButton.className = 'toggle-mobile-button fas fa-mobile-alt';
             toggleButton.onclick = () => {
@@ -194,7 +204,7 @@ class MainController {
             let titleBar = document.querySelector('.header');
             titleBar.appendChild(toggleButton);
         `);
-  }
+    }
 }
 
 module.exports = MainController;
